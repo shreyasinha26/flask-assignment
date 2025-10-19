d7650bf2509ffb39ec179f755975f961c8a44cd7
shreyasinha26 <shreyasin2611@gmail.com>
2025-10-19 18:17:48 +0530

    Initial Assignment1: Flask app + venv ignored

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder="public", static_url_path="/public")
+
+@app.get("/")
+def index():
+    return "Welcome to my REST API!"
+
+@app.get("/api/v1/cat")
+def get_cat():
+    return {
+        "cat_id": "1",
+        "name": "Misu",
+        "birthdate": "2022-03-01",
+        "weight": 4.2,
+        "owner": "12345",
+        "image": "https://place-hold.it/320x240&text=Cat"
+    }
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=3000, debug=True)
