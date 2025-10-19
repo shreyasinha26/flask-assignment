2779246b27804f4b7a2f629d3dbe7ed6ef45f772
shreyasinha26 <shreyasin2611@gmail.com>
2025-10-19 19:27:40 +0530

    port updated

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def get_cat():
         "weight": 4.2,
         "owner": "Paul",
         "image": "http://127.0.0.1:3000/public/cat.png"
+        
     }
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
